854179b337d78bdf002240cf2ef5e609768370f8
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2024-02-20 13:40:12 +0700

    adding jest alias

--- src/lib/Matrix.ts
+++ src/lib/Matrix.ts
@@ -1,8 +1,35 @@
 import * as math from "mathjs";
 
-export function inverseModulo(a: number, m: number): number {}
+export function inverseModuloMatrix(
+  matrix: number[][],
+  mod: number
+): math.Matrix {
+  const adj = getAdjugateMatrix(matrix);
+  const det = math.det(matrix);
+  const detInv = (math as any).invmod(det, mod);
 
-export function getAdjugateMatrix(matrix: math.Matrix): math.Matrix {
+  if (detInv == null || Number.isNaN(detInv)) {
+    throw new Error("matrix doesn't have inverse");
+  }
+
+  const inv = math.multiply(detInv, adj);
+  return inv
+    .map((val) => math.mod(val, mod))
+    .map((val) => (val < 0 ? val + mod : val));
+}
+
+export function multiplyMatrixMod(
+  mat1: math.Matrix,
+  mat2: math.Matrix,
+  mod: number
+): math.Matrix {
+  const result = math.multiply(mat1, mat2);
+  return result
+    .map((val) => math.mod(val, mod))
+    .map((val) => (val < 0 ? val + mod : val));
+}
+
+export function getAdjugateMatrix(matrix: number[][]): math.Matrix {
   const cofactor = getCofactorMatrix(matrix);
   const adjugate = math.transpose(cofactor);
 
@@ -10,14 +37,13 @@ export function getAdjugateMatrix(matrix: math.Matrix): math.Matrix {
 }
 
 export function getMinorMatrix(
-  matrix: math.Matrix,
+  matrix: number[][],
   row: number,
   col: number
 ): math.Matrix {
   const cofactor = [];
-  const arrayMatrix = matrix.toArray();
 
-  for (let i = 0; i < arrayMatrix.length; i++) {
+  for (let i = 0; i < matrix.length; i++) {
     if (i === row) continue;
 
     const rowVal = [];

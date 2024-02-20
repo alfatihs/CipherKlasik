9456d73e5f0fcd38d224e34427cd2d84fa304021
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2024-02-20 14:36:33 +0700

    add hillcipher

--- src/test/HillCipher.test.ts
+++ src/test/HillCipher.test.ts
@@ -1,3 +1,32 @@
+import { TextHillCipher } from "@/lib/cipher/HillCipher";
+import { decodeString, encodeString } from "@/lib/encoder/Encoder";
+
 describe("Test hill cipher", () => {
-  it("should able to encypt and decrypt back", () => {});
+  it("should able to encypt and decrypt back (9 chars)", () => {
+    const hillCipher = new TextHillCipher([
+      [6, 24, 1],
+      [13, 16, 10],
+      [20, 17, 15],
+    ]);
+
+    const plaintext = "kirimuang";
+    const result = hillCipher.encrypt(encodeString(plaintext));
+    const decrypt = decodeString(hillCipher.decrypt(result));
+
+    expect(decrypt).toBe(plaintext);
+  });
+
+  it("should able to encypt and decrypt back (4 chars)", () => {
+    const hillCipher = new TextHillCipher([
+      [6, 24, 1],
+      [13, 16, 10],
+      [20, 17, 15],
+    ]);
+
+    const plaintext = "halo";
+    const result = hillCipher.encrypt(encodeString(plaintext));
+    const decrypt = decodeString(hillCipher.decrypt(result));
+
+    expect(decrypt).toBe(plaintext);
+  });
 });

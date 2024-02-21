6a628239c3ce1ad70a71c650180ef10d05dd8974
Azmi Alfatih S <azmi.alfatihs@gmail.com>
2024-02-21 09:24:42 +0700

    VigenereExtended Tested

--- src/test/VigenereAuto.test.ts
+++ src/test/VigenereAuto.test.ts
@@ -7,7 +7,8 @@ var keyUint = encodeString(keyString);
 describe("Test Vigenere Standard cipher ", () => {
   it("should encrypt correctyly", () => {
     const vigenereCipher = new VigenereAuto(keyUint);
-    const plaintext = "negara penghasil minyak mentah di dunia";
+    const plaintext =
+      "negara penghasil  123123!!--minyak mentah di dunia !2123891237198!";
 
     const result = vigenereCipher.encrypt(encodeString(plaintext));
     var decodedResult = decodeString(result);
---
+++ src/test/VigenereExtended.test.ts
@@ -0,0 +1,31 @@
+// import Vigene from "../lib/cipher/VigenereExtended";
+import VigenereExtended from "@/lib/cipher/VigenereExtended";
+import { decodeString, encodeString } from "../lib/encoder/Encoder";
+
+var keyString = "sony";
+var keyUint = encodeString(keyString);
+
+describe("Test Vigenere Standard cipher ", () => {
+  // it("should encrypt correctyly", () => {
+  //   const vigenereCipher = new VigenereExtended(keyUint);
+  //   const plaintext = "thisplaintext";
+
+  //   const result = vigenereCipher.encrypt(encodeString(plaintext));
+  //   var decodedResult = decodeString(result);
+
+  //   // expect(decodedResult).toBe("LVVQHZNGFHRVL");
+  //   expect(decodedResult).toBe("lvvqhzngfhrvl");
+  // });
+  // permasalahannya di decode nya , decodenya ga bisa ngebaca ASCII yang jauh.
+
+  it("should decrypt correctyly", () => {
+    const vigenereCipher = new VigenereExtended(keyUint);
+    const plaintext = "hellowo 102938120932819 !!! asdasdSADDASKJH -11~rld";
+
+    const result = vigenereCipher.encrypt(encodeString(plaintext));
+    var decodedResult = decodeString(result);
+
+    const decrypt = decodeString(vigenereCipher.decrypt(result));
+    expect(decrypt).toBe(plaintext);
+  });
+});
